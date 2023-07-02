e3aab318132a848c43b8861a2c4b2daecbd40330
robertkosciuszko <114183493+robertkosciuszko@users.noreply.github.com>
2023-07-02 17:08:55 +0200

    ,

--- src/components/redux/actions.js
+++ src/components/redux/actions.js
@@ -14,5 +14,6 @@ export const addContact = createAction(
     };
   }
 );
+
 export const delContact = createAction('contacts/deleteContact');
 export const setFilter = createAction('filter/setFilter');
